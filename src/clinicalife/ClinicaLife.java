73a8e1028a6f8cfc66174b6af1797f9879bdf416
Luciano Lopes <luciano@skyline.local>
2020-08-23 15:44:01 -0300

    correcao look and feel

--- src/clinicalife/ClinicaLife.java
+++ src/clinicalife/ClinicaLife.java
@@ -4,11 +4,19 @@
 
 package clinicalife;
 
+import javax.swing.UIManager;
 import view.Login;
 
 public class ClinicaLife {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         Login login = new Login();
         login.setVisible(true);
     }
